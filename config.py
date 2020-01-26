90ada541488fcd21ddf0c277e41889ccbae95f2c
ss-ghule <ss.ghule@ncl.res.in>
2020-01-26 09:47:21 +0530

    NOTE COMMIT(NEW UPDATE):  * start new version  * bump version no to 2.0.0  * add capability to get adjacency matrix and graph of a frame from sdf/mol file  * rewrite import statements

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-__version__=1.0.3
+__version__=2.0.0
+
+'''
+NOTE COMMIT(NEW UPDATE):
+ * bump version no to 2.0.0
+ * add capability to get adjacency matrix and graph of a frame from sdf/mol file
+ * rewrite import statements 
+'''
