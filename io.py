e40421fe3d7d3c26343e4c6c738df644a368ef82
ss-ghule <ss.ghule@ncl.res.in>
2020-01-28 03:19:10 +0530

    add capability to get adjacency matrix and graph

--- io.py
+++ io.py
@@ -7,6 +7,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import sys
 sys.path.extend(['.'])
 
+import read_file_mol
 import read_file_mol_md
 import read_file_xyz
 import read_file_xyz_md
@@ -22,16 +23,37 @@ def fileType(file):
   else:
     return file.name.strip().split('.')[-1].lower()
 
-def readFile(file_path,file_type=None,info='cords'):
+def readFile(file_path,file_type=None,info='cords',atom_no_digits=3):
   if file_type==None:
     file_type=fileType(file_path)
   if file_type=='xyz':
     if info=='cords':
       df=read_file_xyz.getCords(file_path)
       return df
-    elif info=='atoms':
+    elif info=='atom_count':
       atoms=read_file_xyz.totalAtoms(file_path)
       return atoms
+  elif file_type=='mol':
+    if info=='atom_count':
+      atom_count=read_file_mol.getAtomCount(file_path,atom_no_digits=atom_no_digits)
+      return atom_count
+    elif info=='cords':
+      df=read_file_mol.getCords(file_path,atom_no_digits=atom_no_digits)
+      return df
+    elif info=='bonds':
+      df=read_file_mol.getBonds(file_path,atom_no_digits=atom_no_digits)
+      return df
+    elif info=='cords_and_bonds':
+      dfs=read_file_mol.getCordsAndBonds(file_path,atom_no_digits=atom_no_digits)
+      return dfs
+    elif info=='adjacency_matrix':
+      adjacency_matrix=read_file_mol.getAdjacencyMatrix(file_path,atom_no_digits=atom_no_digits)
+      return adjacency_matrix
+    elif info=='graph':
+      graph=read_file_mol.getFrameGraph(file_path,atom_no_digits=atom_no_digits)
+      return graph
+    else:
+      print('check info')
   elif file_type=='opt':
     if info=='cords':
       df=read_file_opt.getCords(file_path)
@@ -112,9 +134,17 @@ if __name__=='__main__':
   #file_path='/home/vanka/siddharth/shailaja_project/Na_cluster_for_center_of_mass'
   #df=readFile(file_path,file_type='xyz')
   #file_path='/home/vanka/shailja/na_h20_with_in_4angstrom_from_md_now_aimd/scr/coors.xyz'
-  mol_sd_file_path='test_systems/frames_0_1000.mol'
+  #mol_sd_file_path='test_systems/frames_0_1000.mol'
+  '''
   with open(mol_sd_file_path,'r') as file:
     df=readFileMd(file,start_frame_no=0,end_frame_no=0,info='graph',frame_no_pos=2)
     pos=nx.spring_layout(df[0])
     nx.draw_networkx(df[0],pos,labels=nx.get_node_attributes(df[0],'element'))
     plt.show()
+  '''
+  file_path='test_systems/sugar.mol'
+  df=readFile(file_path,info='graph')
+  print(df)
+  pos=nx.spring_layout(df)
+  nx.draw_networkx(df,pos,labels=nx.get_node_attributes(df,'element'))
+  plt.show()
