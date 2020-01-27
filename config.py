8555a6f5f92e8da18c818df9003a52a882a335e0
ss-ghule <ss.ghule@ncl.res.in>
2020-01-28 03:27:26 +0530

    create release-2.0.0 branch

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
 __version__=2.0.0
 
 '''
-NOTE COMMIT(NEW UPDATE):
- * bump version no to 2.0.0
- * add capability to get adjacency matrix and graph of a frame from sdf/mol file
- * rewrite import statements 
+create release-2.0.0
 '''
